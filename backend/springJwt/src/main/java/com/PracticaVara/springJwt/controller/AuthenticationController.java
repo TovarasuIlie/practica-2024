a52775add416268e2c2b387d5ac5d20ba95739ba
Herman Darius-Razvan <darius.herman21@yahoo.com>
2024-07-19 21:23:42 +0300

    Doamne ajuta-ne pe noi

---
+++ backend/springJwt/src/main/java/com/PracticaVara/springJwt/controller/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.PracticaVara.springJwt.controller;
+
+import com.PracticaVara.springJwt.model.AuthenticationResponse;
+import com.PracticaVara.springJwt.model.User;
+import com.PracticaVara.springJwt.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody User request
+    ){
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody User request
+    ){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @PostMapping("/admin_only")
+    public ResponseEntity<String> adminOnly(){
+        return ResponseEntity.ok("Hello from admin only URL");
+    }
+}
---
+++ backend/springJwt/src/main/java/com/PracticaVara/springJwt/model/Role.java
@@ -0,0 +1,6 @@
+package com.PracticaVara.springJwt.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
