bba2af81d88737b9f680397408d2f08dfaafcf2c
Niculai Ilie-Traian <niculaii58@gmail.com>
2024-07-27 14:45:36 +0300

    SSR Removed

--- frontend/src/app/route-guards/auth.guard.ts
+++ frontend/src/app/route-guards/auth.guard.ts
@@ -4,7 +4,6 @@ import { Observable, map } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { ToastService } from '../services/toast.service';
 import { User } from '../models/user';
-import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -19,14 +18,9 @@ export class AuthGuard {
     return this.authService.user$.pipe(
       map((user: User | null) => {
         if(user) {
-          if(isPlatformServer(this.platformId)) {
-            return true;
-          } else {
-            return false;
-          }
+          return true;
         } else {
-          console.log(user);
-          // this.toasterService.show({title: "Acces interzis!", message: "Pentru a intra pe acea pagina trebuie sa fi autentificat!", classname: "text-danger"});
+          this.toasterService.show({title: "Acces interzis!", message: "Pentru a intra pe acea pagina trebuie sa fi autentificat!", classname: "text-danger"});
           this.router.navigateByUrl('/');
           return false;
         }
